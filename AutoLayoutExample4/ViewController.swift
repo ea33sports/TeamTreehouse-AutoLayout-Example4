1c140d892d3e03f91e8d744e35d56b4962acd6a1
Eric Developer <ea33sports@me.com>
2018-08-10 22:38:37 -0500

    Auto Layout Example 4 commit

---
+++ AutoLayoutExample4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample4
+//
+//  Created by Eric Andersen on 8/1/18.
+//  Copyright © 2018 Treehouse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
